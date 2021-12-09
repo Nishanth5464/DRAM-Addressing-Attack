8c4eb3931b1310d0665df27dbe233f48fcf0b094
NISHANTH RB <91217440+Nishanth5464@users.noreply.github.com>
2021-12-09 19:56:48 +0530

    Update host2.cpp

--- Attacktrial/host2.cpp
+++ Attacktrial/host2.cpp
@@ -25,14 +25,68 @@
 
 
 int theSet = 5;
+long long int initialSetupCount = 2000;
+long long int usualCount = 200;
+long long unsigned int syncTime = 1e9;
 int validIndices [100];
 int numValidIndices = 0;
 int arraySize = 1024*1024;
+int timeInterval = 1;
 float fraction_of_physical_memory = 0.4;
+int num_reads = 5000;
+uint64_t rdtsc() {
+    uint64_t a, d;
+    asm volatile ("xor %%rax, %%rax\n" "cpuid"::: "rax", "rbx", "rcx", "rdx");
+    asm volatile ("rdtscp" : "=a" (a), "=d" (d) : : "rcx");
+    a = (d << 32) | a;
+    return a;
+}
+uint64_t rdtsc2() {
+    uint64_t a, d;
+    asm volatile ("rdtscp" : "=a" (a), "=d" (d) : : "rcx");
+    asm volatile ("cpuid"::: "rax", "rbx", "rcx", "rdx");
+    a = (d << 32) | a;
+    return a;
+}
+
+void flush(void* p) {
+    asm volatile ("clflush 0(%0)\n"
+      :
+      : "c" (p)
+      : "rax");
+}
 
 int pagemap = -1;
 uint64_t frame_number_from_pagemap(uint64_t value) {
   return value & ((1ULL << 54) - 1);
+}
+
+uint64_t getTiming(void* first) {
+    size_t min_res = (-1ull);
+    for (int i = 0; i < 4; i++) {
+        size_t number_of_reads = num_reads;
+        volatile size_t *f = (volatile size_t *) first;
+
+        for (int j = 0; j < 10; j++)
+            sched_yield();
+        size_t t0 = rdtsc();
+
+        while (number_of_reads-- > 0) {
+            *f;
+            *(f + number_of_reads);
+
+         
+
+            asm volatile("clflush (%0)" : : "r" (f) : "memory");
+        }
+
+        uint64_t res = (rdtsc2() - t0) / (num_reads);
+        for (int j = 0; j < 10; j++)
+            sched_yield();
+        if (res < min_res)
+            min_res = res;
+    }
+    return min_res;
 }
 
 uint64_t GetPhysicalMemorySize() {
@@ -53,7 +107,6 @@ size_t get_dram_mapping(void* phys_addr_p) {
   static const size_t h1[] = { 14,17 };
   static const size_t h2[] = { 15,18 };
   static const size_t h3[] = { 16,19 };
- 
 
   size_t count = sizeof(h0) / sizeof(h0[0]);
   size_t hash = 0;
@@ -75,7 +128,7 @@ size_t get_dram_mapping(void* phys_addr_p) {
   for (size_t i = 0; i < count; i++) {
     hash3 ^= (phys_addr >> h3[i]) & 1;
   }
- 
+
   return (hash3 << 3) | (hash2 << 2) | (hash1 << 1) | hash;
 }
 
@@ -94,7 +147,7 @@ uint64_t get_physical_addr(uint64_t virtual_addr) {
 int main()
 {
   size_t mapping_size = static_cast<uint64_t>((static_cast<double>(GetPhysicalMemorySize()) * fraction_of_physical_memory));
-  printf("%u\n", mapping_size);
+
   void* mapping = mmap(NULL, mapping_size, PROT_READ | PROT_WRITE,
       MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
   assert(mapping != (void*)-1);
@@ -103,7 +156,7 @@ int main()
   for (uint64_t index = 0; index < mapping_size; index += 0x400) {
     *(uint64_t*) (((char*)mapping) + index) = index * 37;
   }
-  
+
   pagemap = open("/proc/self/pagemap", O_RDONLY);
   assert(pagemap >= 0);
 
@@ -113,6 +166,7 @@ int main()
 	{
 		arr[i] = i;
 	}
+
 	for (int i = 0; i < arraySize; i += 64/sizeof(int)) {
 		size_t setx = get_dram_mapping((void*)get_physical_addr((uint64_t)(arr+i)));
         size_t rowx = get_dram_row((void*)get_physical_addr((uint64_t)(arr+i)));
@@ -121,7 +175,50 @@ int main()
         	numValidIndices++;
         }
     }
+    assert(numValidIndices > 0);
+    // prin
     printf("%d valid indies:\n", numValidIndices);
+    int chosenIndex = validIndices[numValidIndices/2];
+    int totalWaitTime = 0, totalOuterRep = 100;
+    int numrep = 10;
+    long long int sum = 0;
+    long long int count = 0;
 
+    FILE* f = fopen("timing.txt","w");
+    fclose(f);
+
+
+    while(count < initialSetupCount) {
+    	uint64_t time = getTiming(arr + chosenIndex);
+      sum += time;
+      count ++;
+    }
+    printf("Average computed : %f\n", (float) sum/ ((float) count));
+  
+    float average = (float) sum /((float) count);
+
+    long long unsigned int currentTime = rdtsc()/syncTime; 
+
+    while(1)
+    {
+      f = fopen("timing.txt","a");
+      sum = 0;
+      count = 0;
+      while (rdtsc() / syncTime == currentTime)
+      {
+          uint64_t time = getTiming(arr + chosenIndex);
+          sum += time;
+          count ++;
+      }
+      currentTime += 1;
+
+      float currentAverage = (float) sum/((float)count);
+      fprintf(f,"%f\n",currentAverage );
+      printf("%f\n",currentAverage );
+    	bool currentBit = (currentAverage > average + 15.0);
+    	printf("Bit: %d  Avg:%2f Time: %lu\n", currentBit, currentAverage, rdtsc());
+      fclose(f);
+
+    }
 	return 0;
 }
